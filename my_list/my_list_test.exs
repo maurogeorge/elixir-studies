1669d5d440cce722a4de47fcdd8af514450b816f
Mauro George <maurogot@gmail.com>
2013-09-25 20:45:47 -0300

    Better name on tests :watermelon:

--- my_list/my_list_test.exs
+++ my_list/my_list_test.exs
@@ -6,19 +6,19 @@ ExUnit.start
 defmodule MyListTest do
   use ExUnit.Case
 
-  test "len with a empty list" do
+  test "len handle a empty list" do
     assert MyList.len([]) == 0
   end
 
-  test "len with a 2 elements list" do
+  test "len handle 2 elements list" do
     assert MyList.len([1, 2]) == 2
   end
 
-  test "len with a 3 elements list" do
+  test "len handle multiple elements list" do
     assert MyList.len([1, 2, 97]) == 3
   end
 
-  test "len with a list of strings" do
+  test "len handle a list independent of type" do
     assert MyList.len(["foo", "bar", "baz"]) == 3
   end
 end
