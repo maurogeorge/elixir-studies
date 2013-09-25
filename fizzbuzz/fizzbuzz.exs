bc53a2d2a1258a0612c0660061a26c378eea3cc9
Mauro George <maurogot@gmail.com>
2013-09-25 20:20:32 -0300

    make some functions private

--- fizzbuzz/fizzbuzz.exs
+++ fizzbuzz/fizzbuzz.exs
@@ -1,24 +1,24 @@
 defmodule FizzBuzz do
 
-  def fizzbuzz_word(0, 0, _) do
+  def execute(n) do
+    fizzbuzz_word(rem(n, 3), rem(n, 5), n)
+  end
+
+  defp fizzbuzz_word(0, 0, _) do
     "FizzBuzz"
   end
 
   """
   Just to remember the block syntax
   """
-  def fizzbuzz_word(0, _, _), do: "Fizz"
+  defp fizzbuzz_word(0, _, _), do: "Fizz"
 
-  def fizzbuzz_word(_, 0, _) do
+  defp fizzbuzz_word(_, 0, _) do
     "Buzz"
   end
 
-  def fizzbuzz_word(_, _, n) do
+  defp fizzbuzz_word(_, _, n) do
     n
-  end
-
-  def execute(n) do
-    fizzbuzz_word(rem(n, 3), rem(n, 5), n)
   end
 end
 
