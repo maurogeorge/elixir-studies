6fdf126e1451c11d44c9ee0de0e1ac55bb6145f3
Mauro George <maurogot@gmail.com>
2014-08-10 09:45:33 -0300

    Create process on CLI

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -9,7 +9,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+      |> parse_args
+      |> process
   end
 
   @doc """
@@ -32,5 +34,16 @@ defmodule Issues.CLI do
         -> { user, project, @default_count }
       _ -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
