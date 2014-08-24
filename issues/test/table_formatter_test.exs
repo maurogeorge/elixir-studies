b411c10416029ccdf9ac650b55e345d72a6e56c2
Mauro George <maurogot@gmail.com>
2014-08-24 09:58:02 -0300

    Fix broken tests

--- issues/test/table_formatter_test.exs
+++ issues/test/table_formatter_test.exs
@@ -41,12 +41,12 @@ defmodule TableFormatterTest do
       TF.print_table_for_columns(simple_test_data, headers)
     end
     assert result == """
-    c1    | c2     | c4
+    c1    | c2     | c4     
     ------+--------+--------
     r1 c1 | r1 c2  | r1+++c4
-    r2 c1 | r2 c2  | r2 c4
-    r3 c1 | r3 c2  | r3 c4
-    r4 c1 | r4++c2 | r4 c4
+    r2 c1 | r2 c2  | r2 c4  
+    r3 c1 | r3 c2  | r3 c4  
+    r4 c1 | r4++c2 | r4 c4  
     """
   end
 end
